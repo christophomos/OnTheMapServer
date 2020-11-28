0c1d4aee10558803b1c9c97e755d1c62afdd95a9
Chris Clark <chris@qalang.com>
2020-11-28 23:20:46 +0800

    Working hello world Post with JSON request and response

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,6 +9,26 @@ func routes(_ app: Application) throws {
     app.get("hello") { req -> String in
         return "Hello, world!"
     }
+    
+    app.get("hello", ":name") { req -> String in
+        guard let name = req.parameters.get("name") else {
+            throw Abort(.internalServerError)
+        }
+        return "Hello, \(name)!"
+    }
+    
+    app.post("info") { req -> InfoResponse in
+        let data = try req.content.decode(InfoData.self)
+        return InfoResponse(superheroName: "Super\(data.name)")
+    }
 
     try app.register(collection: TodoController())
+}
+
+struct InfoData: Content {
+    let name: String
+}
+
+struct InfoResponse: Content {
+    let superheroName: String
 }
